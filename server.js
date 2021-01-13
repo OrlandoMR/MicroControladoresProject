60b48f0c2f2ba722702f6b1ab514317305ac7acf
Orlando <orlando2170133@correo.uis.edu.co>
2021-01-12 19:34:10 -0500

    update deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/MicroControladoresProject'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function(req,res) {
 
