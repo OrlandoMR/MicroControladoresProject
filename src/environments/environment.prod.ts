06cc9e9b5dadf584bad5feea33e1b45cb2fc6c20
OrlandoMR1 <orlando2170133@correo.uis.edu.co>
2021-03-01 07:56:52 -0500

    firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCEEX-OXfcbKWzCeQfsJ-_f8cSMpCW7mH4",
+    authDomain: "microproject-ee507.firebaseapp.com",
+    databaseURL: "https://microproject-ee507-default-rtdb.firebaseio.com",
+    projectId: "microproject-ee507",
+    storageBucket: "microproject-ee507.appspot.com",
+    messagingSenderId: "309618102045",
+    appId: "1:309618102045:web:83e0b167cb074cf6729bf4",
+    measurementId: "G-K6JR1RFBVR"
+  }
 };
